4efb3926d3dba2f85d41ae9214e01ecf2b8a9fc6
Azeb Tesfay <azbseb@gmail.com>
2025-11-04 11:40:52 -0800

    commit1

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
-import express from 'express';
+import express from 'express'
 
 // Instantiate an express (web) app
-const app = express();
+const app = express()
 
 // Define a port number for the app to listen on
-const PORT = 3008;
+const PORT = 3008
 
 // Tell the app to encode data into JSON format
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 // Set your view (templating) engine to "EJS"
 // (We use a templating engine to create dynamic web pages)
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 // Define a "default" route, 
 // e.g. jshmo.greenriverdev.com/reservation-app/
 app.get('/', (req, res) => {
-	// Log message to the server's console
-	console.log("Hello, world - server!");
+    // Log message to the server's console
+    console.log("Hello, world - server!")
 
     // Return home page
-    res.render('home');
-});
+    res.render('home')
+})
 
 // Define a "confirm" route, using the POST method
 app.post('/confirm', (req, res) => {
     // Get the data from the form that was submitted
     // from the body of the request object
-    let details = req.body;
+    let details = req.body
 
     // Display the confirm page, pass the data
-    res.render('confirm', { details: details });
+    res.render('confirm', { details: details })
 })
 
 // Tell the app to listen for requests on the designated port
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`)
-});
+})
+
+// Define a default "route" ('/)
+app.get('/', (req, res) => {
+    res.render('home', { name: 'Donald Duck' })
+})
